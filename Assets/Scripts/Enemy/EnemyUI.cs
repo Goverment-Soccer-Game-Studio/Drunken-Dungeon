b6992461fc47b23de52b65222ce884eb8a1e773e
RobeJohnGS <jroberts@student.neumont.edu>
2024-07-10 12:38:14 -0600

    Started interactions and UI
    Added test interactables and created some UIs to account for world space and overlay space.

--- Assets/Scripts/Enemy/EnemyUI.cs
+++ Assets/Scripts/Enemy/EnemyUI.cs
@@ -6,15 +6,26 @@ using UnityEngine.UI;
 public class EnemyUI : MonoBehaviour
 {
     [SerializeField] Image healthBar;
+    [SerializeField] Image healthBarBg;
     private IEnemy enemy;
+    Camera mainCam;
 
     private void Awake()
     {
         enemy = gameObject.GetComponent<IEnemy>();
+        mainCam = Camera.main;
     }
 
     private void Update()
     {
         healthBar.fillAmount = enemy.health / 100f;
+        BillboardHP(healthBar);
+        BillboardHP(healthBarBg);
+    }
+
+    private void BillboardHP(Image img)
+    {
+        img.gameObject.transform.LookAt(mainCam.transform.position);
+        img.gameObject.transform.Rotate(0, 180, 0);
     }
 }
