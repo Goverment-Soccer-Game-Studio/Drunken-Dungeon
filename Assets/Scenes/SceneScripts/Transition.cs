510a36fd1de367e15ffc780e04adbae439e0915e
Lorenzo Franco <lfranco@student.neumont.edu>
2024-07-23 19:30:35 -0600

    Big ol Rewards and Boss Push

--- Assets/Scenes/SceneScripts/Transition.cs
+++ Assets/Scenes/SceneScripts/Transition.cs
@@ -3,19 +3,23 @@ using UnityEngine.SceneManagement;
 
 public class DoorTeleport : MonoBehaviour
 {
-    [SerializeField] private string[] sceneNames; // List of scene names to choose from
+    [SerializeField] private string[] sceneNames; // List of regular scene names
+    [SerializeField] private string[] rewardSceneNames; // List of reward scene names
+    [SerializeField] private string[] bossSceneNames; // List of boss scene names
+
+    private static int roomCounter = 0; // Counter to keep track of the number of rooms loaded
 
     private void OnTriggerEnter(Collider other)
     {
         // Check if the player has collided with the door
         if (other.CompareTag("Player"))
         {
-            // Load a random scene from the list
-            LoadRandomScene();
+            // Load the appropriate scene
+            LoadNextScene();
         }
     }
 
-    private void LoadRandomScene()
+    private void LoadNextScene()
     {
         if (sceneNames.Length == 0)
         {
@@ -23,11 +27,32 @@ public class DoorTeleport : MonoBehaviour
             return;
         }
 
-        // Pick a random scene from the list
-        int randomIndex = Random.Range(0, sceneNames.Length);
-        string randomScene = sceneNames[randomIndex];
+        string nextScene;
+
+        // Increment room counter
+        roomCounter++;
+
+        // Determine the type of room to load based on the counter
+        if (roomCounter % 10 == 0 && bossSceneNames.Length > 0)
+        {
+            // Load a boss room every 10 rooms
+            int randomIndex = Random.Range(0, bossSceneNames.Length);
+            nextScene = bossSceneNames[randomIndex];
+        }
+        else if (roomCounter % 3 == 0 && rewardSceneNames.Length > 0)
+        {
+            // Load a reward room every 3 rooms
+            int randomIndex = Random.Range(0, rewardSceneNames.Length);
+            nextScene = rewardSceneNames[randomIndex];
+        }
+        else
+        {
+            // Load a regular room
+            int randomIndex = Random.Range(0, sceneNames.Length);
+            nextScene = sceneNames[randomIndex];
+        }
 
         // Load the chosen scene
-        SceneManager.LoadScene(randomScene);
+        SceneManager.LoadScene(nextScene);
     }
 }
