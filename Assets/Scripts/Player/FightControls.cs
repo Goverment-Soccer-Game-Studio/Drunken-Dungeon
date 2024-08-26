cc5c237f5ce92e35019bbe28b38cd93344ff71de
Matthan Cantrell <cantrellmatthan247@gmail.com>
2024-08-26 10:00:49 -0600

    Small Adjustments With Player Systems

--- Assets/Scripts/Player/FightControls.cs
+++ Assets/Scripts/Player/FightControls.cs
@@ -23,7 +23,7 @@ public class FightControls : MonoBehaviour
         //Left punch
         if (lPunch && lPunchTimer <= 0)
         {
-            //Debug.Log("Left paunch");
+            Debug.Log("Left paunch");
             animator.SetTrigger("LeftPunch");
             lPunchTimer = punchCooldown;
         }
@@ -35,7 +35,7 @@ public class FightControls : MonoBehaviour
         //Right punch
         if (rPunch && rPunchTimer <= 0)
         {
-            //Debug.Log("Right paunch");
+            Debug.Log("Right paunch");
             animator.SetTrigger("RightPunch");
             rPunchTimer = punchCooldown;
         }
